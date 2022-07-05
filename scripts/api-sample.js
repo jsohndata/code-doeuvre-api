f304bc51a45c9a4eac14b367d1ebe816c334cc6c
Jiho Sohn <jihos@me.com>
2022-07-05 14:37:52 -0400

    added items to json
    updated css and other elements

--- scripts/api-sample.js
+++ scripts/api-sample.js
@@ -4,17 +4,22 @@ const APIURL = 'http://localhost:4000/json'
 
 const renderHtml = (paramWhere, paramWhat) =>  document.querySelector(paramWhere).innerHTML = paramWhat
 
-let dataContainer
+let dataContainer = ""
 const processDataEach = paramProcessEach => dataContainer += dataContainerEach(paramProcessEach)
 
 const dataContainerEach = paramContainerEach => 
 `<div class="menu-items__item">
-    <img src="${paramContainerEach.image}">
-    <div>
-        <h3>${paramContainerEach.id} ${paramContainerEach.title}</h3>
+    <article>
+        <img src="${paramContainerEach.image}">
+        <h2>${paramContainerEach.title}</h2>
         <p>${paramContainerEach.description}</p>
-        <p>${paramContainerEach.ingredients}</p>
-    </div>
+        <p><strong>Common Name</strong>: ${paramContainerEach['common name']}</p>
+        <p><strong>Sun</strong>: ${paramContainerEach.sun}</p>
+        <p><strong>Soil</strong>: ${paramContainerEach.soil}</p>
+        <p><strong>Water</strong>: ${paramContainerEach.water}</p>
+        <p><strong>Native</strong>: ${paramContainerEach.native}</p>
+        <p><strong>Reference</strong>: <a href="${paramContainerEach['content source']}" target="_blank">Content source.</a></p>
+    </article>
 </div>`
 
 
